3a4c752352b91b718565fe92bc81b25f5c1b0b64
AndyAjiatas07 <aajiatas-2021496@kinal.edu.gt>
2026-02-17 10:23:01 -0600

    Completación de la sección de Domain y Modelo de datos

--- src/AuthService.Domain/Entities/Role.cs
+++ src/AuthService.Domain/Entities/Role.cs
@@ -1,13 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 namespace AuthService.Domain.Entities;
 public class Role {
-    [ket]
+    [Key]
     [MaxLength(16)]
     public string Id {get; set; }
-    [required]
-    [MawLength(50)]
+    [Required]
+    [MaxLength(50)]
     public string Name {get; set; }
-    [required]
+    [Required]
     [MaxLength(255)]
     public string Description {get; set;}
     public ICollection<UserRole> UserRoles {get;set;}
--- src/AuthService.Domain/Entities/UserPasswordReset.cs
+++ src/AuthService.Domain/Entities/UserPasswordReset.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AuthService.Domain. Entities;
 public class UserPasswordReset
 {
